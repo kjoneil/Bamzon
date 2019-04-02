4c9e4ee0803165da27fd248ccc907bf97be28234
kjoneil <kjoneil.rw@gmail.com>
2019-04-02 18:08:37 -0500

    Created the product database that will be used for the bamzon app

---
+++ bamzon.sql
@@ -0,0 +1,19 @@
+drop database if exists bamazon_db;
+create database bamazon_db;
+
+use bamazon_db;
+
+create table products (
+id integer(15) auto_increment not null,
+product_name varchar(30) not null,
+department_name varchar(45) null,
+price decimal(10,2),
+stock_quantity int null,
+primary key(id)
+);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Socks", "Cloths", 4.50, 100),("Pants", "Cloths", 45.00, 30), ("Shirts", "Cloths", 25.00, 50),
+("Hats", "Cloths", 15.00, 25),("Wallet", "Accessories", 15.75, 10),("Clutch", "Accessories", 22.50, 10),
+("Jacket", "Outerwear", 75.00, 16),("Coat", "Outerwear", 92.99, 12),("Watch", "Accessories", 35.75, 15),
+("Umbrella", "Accessories", 65.00, 10);
